3ed3d8719e60a0ccfaf034092b79daeb4d2fab46
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-07-03 20:33:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", ))
+    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6356388726:AAGNT3VwYvMQ55KGndcUfQ9uQ-Cz-Dw7Yvg")
+    API_ID = int(os.environ.get("APP_ID", 21203142))
+    API_HASH = os.environ.get("API_HASH", "64d92910619503d54fdb9d89ccb805a6")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001924268233))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://ronoxe1308:nulcnaRvqNC5uty@rename-telegram-bot-log.clbv64f.mongodb.net/?retryWrites=true&w=majority")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "xenon")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 5190902724))
